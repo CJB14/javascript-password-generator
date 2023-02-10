475b662a11841dd06975321e4ae0638957cb9052
CJB14 <119978168+CJB14@users.noreply.github.com>
2023-02-10 17:14:19 -0500

    foundation for functional js code

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+//Assignment Code
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
+var generateBtn= document.querySelectorAll("#genrate")
+
+var digits = prompt ("How maany digits do you want your password?");
+var lower = prompt("Do you want lower case? y or n");
+var upper = prompt("Do you want you want upper case? y or n");
+var numeric = prompt("Do you want numeric characters? y or n");
+var syn = prompt ("Do you want symbol? y or n");
+
+console.log(digits,lower,upper,numeric,syn);
+
+//Write password to the #password input
+
+function WritePassword(){
+
+  var password = generatepassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+  passwordText.vaalue = password;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//add event listener to generte button
+
+generateBtn.addEventLlisterner("click",WritePassword);
+
+
+
+const charactersUpper = ['A','B','C','D','E']
