4bc1f08323c81ae3fbb1286a756fe343a82a7ce2
CJB14 <119978168+CJB14@users.noreply.github.com>
2023-02-11 18:07:08 -0500

    prompt show before page loads

--- script.js
+++ script.js
@@ -1,43 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Randomize integer from min - max
-function randomInt(min,max){
-  if (!max) {
-    max=min;
-    min=0;
-  }
-
-  var random = Math.random();
-  return Math.floor(min*(1-random)+ random*max)
-}
-
-  function getRandomItem(list){
-    return list[randomInt(list.length)];
-  }
 
 // Generate passcode
   function generatePassword(){
-    var userInput = window.prompt("how many characters do you want your password");
+    
+    var passlen = window.prompt("how many characters do you want your password");
 
-    var passwordlenghth = parseInt(userInput);
-
-    if (isNaN(passwordlenghth)){
-      window.alert("Please insert number!");
-    }
-
-    If (passwordlenghth < 8 || passwordlenghth > 128); {
+    If (passlen < 8 || passlen > 128) 
     window.alert("Password length must be between 8 and 128 characters")
+    return;
     }
-
-
-
-    // what variables they want in their password
-var userNumbers = window.confirm ("Would you like to include numbers?")
-var userSymbols = window.confirm("Would you like to include symbols")
-var userLowercase = window.confirm("Would you like to include lowecase")
-var userUppercase = window.confirm("Would you like to include uppercase")
-
 // list password criteria
 
 var numberList =  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -45,10 +18,16 @@ var symbolList = ["!", "@", "#", "$", "%", "^", "*"]
 var lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var uppercaseList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
+// what variables they want in their password
+var userNumbers = window.confirm ("Would you like to include numbers?")
+var userSymbols = window.confirm("Would you like to include symbols")
+var userLowercase = window.confirm("Would you like to include lowecase")
+var userUppercase = window.confirm("Would you like to include uppercase")
+
 // user options after answered promts
-var useroptions =[];
+var userOptions =[];
 for (var i = 0; i < lowercaseList.length; i++){
-  uppercaseList(i) = lowercaseList(i). toUppercase();
+  uppercaseList(i) = lowercaseList(i). touppercase();
 }
 
 // user answers true to confirm statements
@@ -71,15 +50,16 @@ if (userOptions.length === 0){
 }
 
 // empty string fill with password
-generatedPassword = "";
+generatePassword = "";
 
 // Randomize chosen items
 
-for (var i = 0; i < passwordlenghth; i++){
+for (var i = 0; i < passlen; i++){
   var randomList = getRandomItem(userOptions);
   var randomchar = getRandomItem(randomList);
 
-  generatedPassword += randomchar
+  generatePassword += randomchar
 }
-  }
-
+  
+// Add event listener to generate button
+generateBtn.addEventListener("click", generatePassword);
