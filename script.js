4a88257c16233df52841151ce809ee2b6852a7ce
CJB14 <119978168+CJB14@users.noreply.github.com>
2023-02-11 16:06:51 -0500

    Modified js to meet criteria

--- script.js
+++ script.js
@@ -1,64 +1,85 @@
-document.getElementById('generate').addEventListener('click', function() {
-  alert(generatePassword());
-});
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
-//Password Criteria
-passwordOptions = {
-number : "1234567890",
-symbol : "!@#$%&",
-uppercase : "abcdefghijklmnopqrstuvwxyz",
-lowercase : "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+// Randomize integer from min - max
+function randomInt(min,max){
+  if (!max) {
+    max=min;
+    min=0;
+  }
 
-//Variables they want in password
-getnumbers: window.confirm("Would you like to include numbers in your password?"),
-getsymbol: window.confirm("Would you like to include symbols in your password?"),
-getuppercase: window.confirm("Would you like to include uppercase letters in your password?"),
-getLowerCase: window.confirm("Would you like to include lowercase letters in your password?")
-
-};
-
-// Generate password
-generatePassword = function(){
- var passwordlength = window.prompt("Enter the amount of characters you want for your password.");{
-    
-// Check to see if password length is correct
-if (passwordlength >= 8 && characterAmount <= 128){
-  window.alert("password must be between 8 and 128 characters")
-
-}}
-
-
-// Ask to include numbers
-const  getnumber = window.confirm("Would you like to include numbers in your password?");{
-  
-  if (getnumber){
-    passInfo += passwordOptions.number;
-    passChars.push(getRandomChar(passwordOptions.number));
-  };
+  var random = Math.random();
+  return Math.floor(min*(1-random)+ random*max)
 }
-// Ask to include Symbol
-const getsymbol= window.confirm("Would you like to include symbols in your password?");
 
-if (getsymbol) {
-  passInfo += passwordOptions.symbol;
-  passChars.push(getRandomChar(passwordOptions.symbol));
-};
+  function getRandomItem(list){
+    return list[randomInt(list.length)];
+  }
 
-// Ask to include lowercase
-const getlowerCase = window.confirm("Would you like to include lowercase letters in your password?");
+// Generate passcode
+  function generatePassword(){
+    var userInput = window.prompt("how many characters do you want your password");
 
-if (getlowerCase) {
-  passInfo += passwordOptions.lowerCase;
-  passChars.push(getRandomChar(passwordOptions.lowerCase));
-};
+    var passwordlenghth = parseInt(userInput);
 
-// Ask to include uppercase
+    if (isNaN(passwordlenghth)){
+      window.alert("Please insert number!");
+    }
 
-const getuppercase = window.confirm("Would you like to include uppercase letters in your password?");
+    If (passwordlenghth < 8 || passwordlenghth > 128); {
+    window.alert("Password length must be between 8 and 128 characters")
+    }
 
-if (getuppercase){
-  passInfo += passwordOptions.uppercase;
-  passChars.push(getuppercase(passwordOptions.uppercase));
 
+
+    // what variables they want in their password
+var userNumbers = window.confirm ("Would you like to include numbers?")
+var userSymbols = window.confirm("Would you like to include symbols")
+var userLowercase = window.confirm("Would you like to include lowecase")
+var userUppercase = window.confirm("Would you like to include uppercase")
+
+// list password criteria
+
+var numberList =  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+var symbolList = ["!", "@", "#", "$", "%", "^", "*"]
+var lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var uppercaseList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+// user options after answered promts
+var useroptions =[];
+for (var i = 0; i < lowercaseList.length; i++){
+  uppercaseList(i) = lowercaseList(i). toUppercase();
 }
+
+// user answers true to confirm statements
+if (userNumbers === true){
+  userOptions.push(numberList);
 }
+if (userSymbols === true){
+  userOptions.push(symbolList);
+}
+if (userLowercase === true){
+  userOptions.push(lowercaseList);
+}
+if (userUppercase === true){
+  userOptions.push(uppercaseList);
+}
+
+// add lowercase if respose is false
+if (userOptions.length === 0){
+  userOptions.push(lowercaseList);
+}
+
+// empty string fill with password
+generatedPassword = "";
+
+// Randomize chosen items
+
+for (var i = 0; i < passwordlenghth; i++){
+  var randomList = getRandomItem(userOptions);
+  var randomchar = getRandomItem(randomList);
+
+  generatedPassword += randomchar
+}
+  }
+
