28c33eaee1937b1d12e0bb1b6ed27d89a7b536a3
CJB14 <119978168+CJB14@users.noreply.github.com>
2023-02-12 08:52:58 -0500

    added function to generate password

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// randomize integer
+function randomInt(min,max){
+if (!max){
+  max = min;
+  min = 0;
+}
+
+var random = Math.random();
+return Math.floor(min*(1 - random) + random*max)
+}
+
+
 // Generate passcode
   function generatePassword(){
-    
+  
+
     var passlen = window.prompt("how many characters do you want your password");
 
     if (passlen < 8 || passlen > 128) 
     window.alert("Password length must be between 8 and 128 characters")
     
 // list password criteria
-
 var numberList =  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 var symbolList = ["!", "@", "#", "$", "%", "^", "*"]
 var lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
@@ -45,16 +57,16 @@ if (userOptions.length === 0){
   userOptions.push(lowercaseList);
 }
 // empty string fill with password
+
 var generatedPassword = "";
 
 // Randomize chosen items
 
-for (var i = 0; i < passlen; i++)
-  var randomList = userOptions;
-  var randomchar = randomList;
-
-  generatedPassword += randomchar
-
+for (var i = 0; i < passlen; i++){
+var randomList = randomInt(userOptions);
+var randomchar = randomInt(randomList);
+generatedPassword += randomchar
+}
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", generatePassword);
