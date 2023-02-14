26c3694922157e82372b8fc66f7709d02f8bd05f
CJB14 <119978168+CJB14@users.noreply.github.com>
2023-02-13 20:20:18 -0500

    password text now deploys with no issue

--- script.js
+++ script.js
@@ -1,72 +1,83 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// randomize integer
-function randomInt(min,max){
-if (!max){
-  max = min;
-  min = 0;
-}
-
-var random = Math.random();
-return Math.floor(min*(1 - random) + random*max)
-}
-
-
-// Generate passcode
-  function generatePassword(){
-  
-
-    var passlen = window.prompt("how many characters do you want your password");
-
-    if (passlen < 8 || passlen > 128) 
-    window.alert("Password length must be between 8 and 128 characters")
-    
-// list password criteria
-var numberList =  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-var symbolList = ["!", "@", "#", "$", "%", "^", "*"]
-var lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+// define all characters
 var uppercaseList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var numberList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+var symbolList = ["!", "@", "#", "$", "%", "^", "*"]
 
-// what variables they want in their password
-var userNumbers = window.confirm ("Would you like to include numbers?")
-var userSymbols = window.confirm("Would you like to include symbols")
-var userLowercase = window.confirm("Would you like to include lowecase")
-var userUppercase = window.confirm("Would you like to include uppercase")
+// empty string to create charset and password variables
+var charset = "";
+var password = "";
 
-//Emty array options
-var userOptions =[]
+// Write password to the #password input
+function writePassword() {
+var password = generatePassword();
+var passwordText = document.querySelector("#password");
 
+passwordText.value = password;
 
-// user answers true to confirm statements
-if (userOptions === true){
-  userOptions.push(numberList);
-}
-if (userSymbols === true){
-  userOptions.push(symbolList);
-}
-if (userLowercase === true){
-  userOptions.push(lowercaseList);
-}
-if (userUppercase === true){
-  userOptions.push(uppercaseList);
 }
 
-// add lowercase if respose is false
-if (userOptions.length === 0){
-  userOptions.push(lowercaseList);
-}
-// empty string fill with password
 
-var generatedPassword = "";
+// password generator logic
+var generatePassword = function() {
+var passLength = 0;
+// check password length parameter
+passLength = prompt("Select a password length: Mininum 8, Maximum 128.")
 
-// Randomize chosen items
+if (passLength >= 8 || passLength <= 128) {
+console.log(passLength);
+} else {
+alert("Please be sure your response is a number between 8 and 128.")
+return "";
+}
 
-for (var i = 0; i < passlen; i++){
-var randomList = randomInt(userOptions);
-var randomchar = randomInt(randomList);
-generatedPassword += randomchar
+// check for uppercase
+var passTypeUpper = confirm("Do you want to include UPPERCASE letters?")
+// add uppercase to charset if wanted
+if (passTypeUpper) {
+charset += uppercaseList
 }
+
+
+// check for lowercase
+var passTypeLower = confirm("Do you want to include lowercase letters?")
+// add lowercase to charset if wanted
+if (passTypeLower) {
+charset += lowercaseList
 }
+
+
+// check for numbers
+var passTypeNum = confirm("Do you want to include numbers?")
+// add numbers to charset if wanted
+if (passTypeNum) {
+charset += numberList
+}
+
+
+// check for specials
+var passTypeSpec = confirm("Would you like to include symbols")
+// add specials to charset if wanted
+if(passTypeSpec) {
+charset += symbolList
+}
+
+
+if (!passTypeUpper && !passTypeLower && !passTypeNum && !passTypeSpec){
+alert("You must select at least one set of characters.")
+return "";
+}
+
+// iterator to run through charset and select the password
+for (var i = 0; i < passLength; i++) {
+password += charset[Math.floor(Math.random() * charset.length)]
+};
+
+return password;
+}
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", generatePassword);
+generateBtn.addEventListener("click", writePassword);
